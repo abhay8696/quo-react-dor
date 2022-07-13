e20720e9abd3d783b6d12a9154ed55867444b501
abhay8696 <abhaykamble1346@gmail.com>
2022-07-13 22:26:51 +0530

    created new rows and columns for box and walls

--- src/components/board.js
+++ src/components/board.js
@@ -17,6 +17,83 @@ const Board = () => {
         setNext(arr);
     },
     dispBoxes = ()=> {
+        let rows = [], i=0;
+        for(let k=1; k<=11; k++){
+            if(k%2===1) i++ ;
+            rows.push(
+                <div className='row' key={`R${k}`}>
+                    { k%2 === 1 ? boxRow(i) : wallRow(i) } 
+                </div>
+            )
+        }
+        return rows;
+    },
+    boxRow = i=> {
+        let row = [], j=0;
+        for(let l=1; l<=11; l++){
+            if(l%2 === 1) j++;
+            l%2 === 1 ? row.push(box(i,j)) : row.push(verticalWall());
+        }
+        return row;
+    },
+    wallRow = (i)=> {
+        let row = [];
+        for(let m=1; m<=6; m++){
+            row.push(horizontalWall());
+        }
+        return row;
+    },
+    horizontalWall = ()=> {
+        return <div className='horizontalWall'></div>
+    },
+    verticalWall = ()=> {
+        return <div className='verticalWall'></div>
+    },
+    box = (i,j)=> {
+        return (<div 
+            className={next.includes(`${i}${j}`) ? 'next' : 'box'}
+            key={`${i}${j}`} 
+            onClick={()=> clickBox(i,j)}
+            >
+                {selected===`${i}${j}` ? <img src={pawn} /> : <></>}
+            </div>)
+    };
+
+    return (
+        <div className='board'>
+            {dispBoxes()}
+        </div>
+    );
+};
+
+export default Board;
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const Board = () => {
+    //states
+    const 
+    [selected, setSelected] = useState(),
+    [next, setNext] = useState([]);
+    //functions
+    const 
+    clickBox = (i,j)=> {
+        setSelected(`${i}${j}`);
+        let t = `${i-1}${j}`, tr=`${i-1}${j+1}`, r=`${i}${j+1}`, br=`${i+1}${j+1}`, b=`${i+1}${j}`, bl=`${i+1}${j-1}`, l=`${i}${j-1}`, tl=`${i-1}${j-1}`;
+        let arr = [t, r, b, l];
+        setNext(arr);
+    },
+    dispBoxes = ()=> {
         let rows = [];
         for(let i=1; i<=6; i++){
             rows.push(
@@ -41,13 +118,18 @@ const Board = () => {
             )
         }
         return row;
-    };
+    },
+    wall = ()=> {
+        return <div className='horizontalWall'></div>
+    }
 
     return (
         <div className='board'>
+            {wall()}
             {dispBoxes()}
         </div>
     );
 };
 
 export default Board;
+*/
