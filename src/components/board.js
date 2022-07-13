c2012335e11bad0b6eec985794f5a3e81005d44c
abhay8696 <abhaykamble1346@gmail.com>
2022-07-13 21:15:33 +0530

    user can select the box and boxes for next moves are highlighted

--- src/components/board.js
+++ src/components/board.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+//styles
 import '../styles/board.css'
+//icons
+import pawn from '../../src/icons/pawn.svg'
 const Board = () => {
-
+    //states
+    const 
+    [selected, setSelected] = useState(),
+    [next, setNext] = useState([]);
     //functions
     const 
+    clickBox = (i,j)=> {
+        setSelected(`${i}${j}`);
+        let t = `${i-1}${j}`, tr=`${i-1}${j+1}`, r=`${i}${j+1}`, br=`${i+1}${j+1}`, b=`${i+1}${j}`, bl=`${i+1}${j-1}`, l=`${i}${j-1}`, tl=`${i-1}${j-1}`;
+        let arr = [t, r, b, l];
+        setNext(arr);
+    },
     dispBoxes = ()=> {
         let rows = [];
         for(let i=1; i<=6; i++){
@@ -20,9 +32,11 @@ const Board = () => {
         for(let j=1; j<=6; j++){
             row.push(
             <div 
-            className='box' 
+            className={next.includes(`${i}${j}`) ? 'next' : 'box'}
             key={`${i}${j}`} 
-            onClick={()=> console.log(`${i}${j}`)}>
+            onClick={()=> clickBox(i,j)}
+            >
+                {selected===`${i}${j}` ? <img src={pawn} /> : <></>}
             </div>
             )
         }
