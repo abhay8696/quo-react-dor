0d977fd17ea4487642ff589135dc2775b0fa734a
abhay8696 <abhaykamble1346@gmail.com>
2022-11-12 22:01:14 +0530

    Updated styles of guide component

--- src/components/guide/Guide.js
+++ src/components/guide/Guide.js
@@ -1,30 +1,66 @@
 import React, { useEffect, useState } from 'react';
-import Step1 from './step1';
-import Step2 from './step2';
-import Step3 from './step3';
-import Step4 from './step4';
-import Step5 from './step5';
-import StepWinner from './stepWinner';
 //styles
 import '../../styles/guide/guide.css'
 //mui
 import { Grow } from '@mui/material';
+import BoardImage from './boardImage';
 
 const Guide = props => {
     const { disappearThis } = props;
     //states
     const [grow, setGrow] = useState(false);
+    const [grow1, setGrow1] = useState(false);
 
     //life cycle
     useEffect(()=> {
         const t = setTimeout(() => {
             handleGrow(true);
+            handleGrow1(true);
         }, 1000);
         return ()=> clearTimeout(t);
     }, [])
 
+    const texts = [
+        '',
+        'Start At One End And Reach Other',
+        'Coin/Pawn Can Move One Step In Adjacent Sqaures',
+        'Use Walls As Obstacles For Opponent',
+        'Coin/Pawn Cannot Pass Through Walls',
+        'Coin/Pawn Should Have Atleast One Way To Reach Destination',
+        'First Coin/Pawn To Reach Opposite Side Wins!',
+    ]
+    const boardArray = [
+        '',
+        <BoardImage noWalls = {true} start_finish = {true}/>,
+        <BoardImage noWalls = {true} moves = {true}/>,
+        <BoardImage moves3={true}/>,
+        <BoardImage moves4={true}/>,
+        <BoardImage moves5={true}/>,
+        <BoardImage movesW={true}/>
+    ]
     const handleGrow = val=> {
         setGrow(prev=> val);
+    }
+    const handleGrow1 = val=> {
+        setGrow1(prev=> val);
+    }
+    const showBoards = ()=> {
+        let arr = [];
+        for(let i=1; i<=6; i++){
+            arr.push(
+                <Grow
+                in={grow1}
+                style={{ transformOrigin: '0 0 0' }}
+                {...(grow1 ? { timeout: 1000 } : {})}
+                >
+                    <div className='stepBody'>
+                        {boardArray[i]}
+                        <span className='text'>{texts[i]}</span>
+                    </div>
+                </Grow>
+            )
+        }
+        return arr;
     }
     return (
         
@@ -32,22 +68,26 @@ const Guide = props => {
             <Grow
             in={grow}
             style={{ transformOrigin: '0 0 0' }}
-            {...(grow ? { timeout: 500 } : {})}
+            {...(grow ? { timeout: 100 } : {})}
             >
-                <div className='header'>How To Play?</div>
+                <div className='howToPlay'>How To Play?</div>
             </Grow>
             <div className='guideBody'>
-                {/* <Step1 disappearThis={disappearThis}/>
-                <Step2 disappearThis={disappearThis}/>
-                <div className='combinedSteps'>
-                    <Step3 disappearThis={disappearThis}/>
-                    <Step4 disappearThis={disappearThis}/>
-                </div>
-                <Step5/>
-                <StepWinner/> */}
+                {showBoards()}
             </div>
         </div>
     );
 };
 
 export default Guide;
+
+
+
+// <Step1 disappearThis={disappearThis}/>
+// <Step2 disappearThis={disappearThis}/>
+// <Step3 disappearThis={disappearThis}/>
+// <Step4 disappearThis={disappearThis}/>
+// <div className='combinedSteps'>
+// </div>
+// <Step5/>
+// <StepWinner/>
