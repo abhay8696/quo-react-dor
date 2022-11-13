6127e7a926bf858cd426e49f3c1fee29f087f0c3
abhay8696 <abhaykamble1346@gmail.com>
2022-11-13 21:56:38 +0530

    updated style for 1vs1 input form

--- src/components/offlineMode/offlineComp.js
+++ src/components/offlineMode/offlineComp.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 //contexts
 import { PlayerDataContext } from '../../contexts/playerDataContext';
+import { OfflineContext } from '../../contexts/offlineCOntext';
 //components
 import Guide from '../guide/Guide';
 //styles
@@ -16,6 +17,7 @@ const OfflineComp = props => {
     const { exitMatch, gameData, updateGameData } = props;
     //context
     const [playerData, setPlayerData] = useContext(PlayerDataContext);
+    const [offlineMode, setOfflineMode] = useContext(OfflineContext);
     //states
     const [name1, setName1] = useState(playerData?.name);
     const [name2, setName2] = useState('');
@@ -36,8 +38,11 @@ const OfflineComp = props => {
 
     return (
         <div className='offlineComp'>
-            <div className='offlineTitle'>1 VS 1</div>
-            <form onSubmit={(e) => handleSubmit(e)} className="form">
+            <div className='offlineBar'>
+                <span>One Screen 1vs1</span>
+                <button className='closeOfflineComp' onClick={()=> setOfflineMode(!offlineMode)}>X</button>
+            </div>
+            <form onSubmit={(e) => handleSubmit(e)} className="inputForm">
                 <input
                     onChange={(e) => setName1(e.target.value)}
                     className="input"
@@ -45,6 +50,7 @@ const OfflineComp = props => {
                     required
                     value={name1}
                     placeholder= {"Player1 Name"}
+                    autoComplete='off'
                 />
                 <div>VS</div>
                 
@@ -55,9 +61,10 @@ const OfflineComp = props => {
                     required
                     value={name2}
                     placeholder= {"Player2 Name"}
+                    autoComplete='off'
                 />
                 <button className="arrow" id="arrowIn" type="submit">
-                    <FaArrowRight />
+                    Start Playing &nbsp;<FaArrowRight />
                 </button>
             </form>
         </div>
